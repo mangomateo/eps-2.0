055bbabcb21b7d2a1dd7e8b9741c2fc060966b3e
Matthew Manzano <velarde.matthew@gmail.com>
2018-08-17 21:04:11 -0400

    Created day visit rate page

--- dayvisits.php
+++ dayvisits.php
@@ -16,7 +16,7 @@
       <div class="row my-5"> <!-- Header: Services & Rates -->
         <div class="col">
           <h1 class="text-center">Services & Rates</h1>
-          <h4 class="text-center mt-5 mb-2">In Home Pet Sitting</h4>
+          <h4 class="text-center mt-5 mb-2">Day Time Visits</h4>
         </div>
       </div> <!-- /Header: Services & Rates -->
 
@@ -26,54 +26,61 @@
 
         <div class="col"> <!-- accordionDiv -->
 
-          <p class="mb-5">In home pet sitting offers your pet the opportunity to stay at home while you’re away. Forget the stress and anxiety of leaving your pet in an unknown facility with other unknown animals. The comfort of home is the comfort your pets desire! We are happy to try and accommodate all pets and work to develop a plan specific to each pet’s needs that keeps your mind at ease during your absence. For this reason, pricing is based on the needs of your pet and will be negotiated for each individual family. </p>
+          <p class="mb-5">Being away for long periods of time is sometimes unavoidable, but it can be hard on our pets. Even if they don’t need a full day of daycare or midday walk, a midday visit can mean the world to some pets. We can help keep your pets happy and healthy with just a short visit, to brighten up their day until you’re home!</p>
 
           <!--Accordion wrapper-->
           <div class="accordion mb-5" id="accordionEx" role="tablist" aria-multiselectable="true">
 
 
-            <!-- Accordion card 1: Part-Time Care -->
+            <!-- Accordion card 1: Puppy Visits -->
             <div class="card">
 
                 <!-- Card header -->
                 <div class="card-header" role="tab" id="headingOne">
                     <a data-toggle="collapse" data-parent="#accordionEx" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
                         <h3 class="mb-0 text-center category">
-                            Part-Time Care <i class="fa fa-angle-down rotate-icon"></i>
+                            Puppy Visits <i class="fa fa-angle-down rotate-icon"></i>
                         </h3>
+                        <h6 class="text-center">(Up to 6 months)</h6>
                     </a>
                 </div>
 
                 <!-- Card body -->
                 <div id="collapseOne" class="collapse" role="tabpanel" aria-labelledby="headingOne" data-parent="#accordionEx">
                     <div class="card-body">
-                      <p>Part-Time Care may be just what your pet needs!</p>
+                      <p>It is crucial for your puppy's training and development that your puppy is not alone for more than 3 hours at a time. That's a huge commitment! When you're not available throughout the day, we can help!</p>
 
-                      <p>Our most popular Part-Time package includes:</p>
+                      <p>Puppy visits include 30 minutes of play time and a potty break (or two). Don't take this at face value though! Every chance your puppy has to play is a chance to learn! </p>
 
                       <ul>
-                        <li>3 - hour long visits (Morning, Afternoon, Evening)</li>
-                        <li>2 - 1/2 hour walks (Morning and Evening)</li>
-                        <li>Meal distribution</li>
-                        <li>Fresh water at every visit</li>
-                        <li>Medication administration</li>
-                        <li>Play time</li>
-                        <li>Potty breaks</li>
-                        <li>Unlimited love and affection</li>
+                        <li>30 Minute Puppy Visit - $18</li>
                       </ul>
+
+                      <p>Going to be gone all day?</p>
+
+                      <ul>
+                        <li>Each additional visit in a day - $10</li>
+                      </ul>
+
+                      <h4 class="mt-4">Package Discount</h4>
+
+                      <ul>
+                        <li>30 Minute Visit x 20 - $15</li>
+                      </ul>
+
                     </div>
                 </div> <!-- /Card body -->
             </div>  <!-- Accordion card -->
 
 
-            <!-- Accordion card 2: Full-Time Care -->
+            <!-- Accordion card 2: Dog & Cat Visits -->
             <div class="card">
 
               <!-- Card header -->
               <div class="card-header" role="tab" id="headingTwo">
                   <a data-toggle="collapse" data-parent="#accordionEx" href="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
                       <h3 class="mb-0 text-center">
-                          Full-Time Care <i class="fa fa-angle-down rotate-icon"></i>
+                          Dog & Cat Visits <i class="fa fa-angle-down rotate-icon"></i>
                       </h3>
                   </a>
               </div>
@@ -81,60 +88,27 @@
               <!-- Card body -->
               <div id="collapseTwo" class="collapse" role="tabpanel" aria-labelledby="headingTwo" data-parent="#accordionEx">
                   <div class="card-body">
-                    <p>For pets that need someone close through the night, we are happy to offer full-time care options.</p>
-
-                    <p>Our most popular full-time package includes:</p>
+                    <p>Give your fido or feline a break from the hum drum of counting down to your return. This is also a great package for those pets that need medicine administration throughout the day.</p>
 
                     <ul>
-                      <li>Guaranteed company from 7:00 PM - 7:00 AM (overnight stay)</li>
-                      <li>1 - 1 hour mid-day visit</li>
-                      <li>2 - 1/2 hour walks (Morning and Evening)</li>
-                      <li>Meal distribution</li>
-                      <li>Fresh water at every visit</li>
-                      <li>Medication administration</li>
-                      <li>Play time</li>
-                      <li>Potty breaks</li>
-                      <li>Unlimited love and affection</li>
+                      <li>30 Minute Visit - $20</li>
+                    </ul>
+
+                    <p>Looking for multiple visits in a day?</p>
+
+                    <ul>
+                      <li>Each additional visit in a day - $12</li>
+                    </ul>
+
+                    <h4 class="mt-4">Package Discount</h4>
+
+                    <ul>
+                      <li>30 Minute Visit X 20 - $17 / Visit</li>
                     </ul>
 
                   </div> <!-- /Card body -->
               </div>  <!-- Accordion card -->
             </div>
-
-
-            <!-- Accordion card 3: Extras -->
-            <div class="card">
-
-                <!-- Card header -->
-                <div class="card-header" role="tab" id="headingThree">
-                    <a data-toggle="collapse" data-parent="#accordionEx" href="#collapseThree" aria-expanded="true" aria-controls="collapseThree">
-                      <h3 class="mb-0 text-center">
-                          Add Some Extra Love <i class="fa fa-angle-down rotate-icon"></i>
-                      </h3>
-                    </a>
-                </div>
-
-                <!-- Card body -->
-                <div id="collapseThree" class="collapse" role="tabpanel" aria-labelledby="headingThree" data-parent="#accordionEx">
-                  <div class="card-body">
-                    <p>Want to come home to a fresh pet?  Tired of dragging your reluctant dog into the salon to hear there's a 30 minute wait for a simple nail trim? I hear ya! Conveniently, I can help you get all of your basic necessities done in your home! Add some extra love to your package and remove some stress from your life!</p>
-
-                    <p>Add any <em>three</em> items to package for $25!</p>
-
-                    <ul>
-                      <li>Nail Trim - $10</li>
-                      <li>Face Trim - $10</li>
-                      <li>Paw Trim - $10</li>
-                      <li>Brush Out (Coat Condition Dependent) - $12+</li>
-                      <li>Sanitary Trim - $7</li>
-                      <li>Ana Gland Expression - $7</li>
-                    </ul>
-
-                    <p><strong>OR</strong> take care of the whole shebang and add a full groom into your package! (Rates Vary)</p>
-
-                  </div>
-                </div> <!-- /Card body -->
-            </div>  <!-- Accordion card -->
 
           </div> <!--/.Accordion wrapper-->
 
@@ -149,7 +123,7 @@
       <div class="row mt-5"> <!-- HST -->
         <div class="col text-center">
           <h5>*All Prices are Subject to HST.</h5>
-          <p>Contact us <a href="contact.php" class="blueLink">here</a> to inquire further about pet sitting services!</p>
+          <p>Contact us <a href="contact.php" class="blueLink">here</a> to inquire further about day time visits!</p>
         </div>
       </div> <!-- /HST -->
       <?php include ("inc/footer.php"); ?>

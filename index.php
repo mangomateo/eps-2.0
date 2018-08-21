d16cf81ed537e2cbacec5f612e402819396a2791
Matthew Manzano <velarde.matthew@gmail.com>
2018-08-21 10:34:27 -0400

    Uploaded stock images, updated content on index.php

--- index.php
+++ index.php
@@ -21,19 +21,39 @@
 
     <div class="container"> <!-- mainContent -->
       <div class="row mt-5">
-        <div class="col"></div>
-        <div class="col-lg-10">
-          <p class="mb-5 main">Our pets deserve the best, we <em>know</em> that. But it’s so hard to dedicate the amount of time our four-legged family members need on a regular basis. Let’s face it, they can’t come everywhere with us and between work, cooking, cleaning, running the kids around and everything else that comes with having a family- Coco’s walk has been missed... again. When the kids are finally in bed and you’re exhausted at the end of the day, Coco’s eyes are on you, tail wagging, full of pent up energy, asking, “Is it my turn, now?”.</p>
+        <div class="col"></div> <!-- dummy width column -->
+        <div class="col-lg-10"> <!-- content col -->
 
-          <p class="mb-5 main">Imagine knowing that while you’re stuck in traffic on your long commute, running the kids around town, getting groceries, cooking dinner, cleaning the house and attending to all of your other commitments, Coco has been walked, fed, given his medicine and had that nail trim you’ve been meaning to take him for.</p>
 
-          <p class="mb-5 main">Now when you’re exhausted at the end of the day, Coco is ready to curl up on your lap and enjoy the peace and quiet with you. Imagine.</p>
+          <div class="row">
+            <div class="col-md">
+              <img src="img/dog001.jpg" class="img-fluid">
+            </div>
+            <div class="col-md">
+              <p class="mb-5 main">Our pets deserve the best, we <em>know</em> that. But it’s so hard to dedicate the amount of time our four-legged family members need on a regular basis. Let’s face it, they can’t come everywhere with us and between work, cooking, cleaning, running the kids around and everything else that comes with having a family- Coco’s walk has been missed... again. When the kids are finally in bed and you’re exhausted at the end of the day, Coco’s eyes are on you, tail wagging, full of pent up energy, asking, “Is it my turn, now?”.</p>
+            </div>
+          </div>
 
-          <h1 class="text-center my-5 em-text">I CAN HELP!</h1>
+          <div class="row">
+            <div class="col">
+              <p class="mb-5 main">Imagine knowing that while you’re stuck in traffic on your long commute, running the kids around town, getting groceries, cooking dinner, cleaning the house and attending to all of your other commitments, Coco has been walked, fed, given his medicine and had that nail trim you’ve been meaning to take him for.</p>
 
-          <p class="main">It is so important for Coco and Bella to get their daily exercise, both for their health and so that they don’t chew up another pair of shoes, or dig through the dirty hamper to sniff out your socks and underwear out of pure boredom. We do everything we can to keep our kids happy and healthy and the same should go for our furry family.</p>
+              <p class="mb-5 main">Now when you’re exhausted at the end of the day, Coco is ready to curl up on your lap and enjoy the peace and quiet with you. Imagine...</p>
 
-          <p class="mb-5 main">You signed your kids up for soccer and ballet to keep them active, social and entertained and you can sign Bella up for regular walks to do the same for her!</p>
+              <h1 class="text-center my-5 em-text">I CAN HELP!</h1>
+            </div>
+          </div>
+
+          <div class="row">
+            <div class="col-md">
+              <p class="main">It is so important for Coco and Bella to get their daily exercise, both for their health and so that they don’t chew up another pair of shoes, or dig through the dirty hamper to sniff out your socks and underwear out of pure boredom. We do everything we can to keep our kids happy and healthy and the same should go for our furry family.</p>
+
+              <p class="mb-5 main">You signed your kids up for soccer and ballet to keep them active, social and entertained and you can sign Bella up for regular walks to do the same for her!</p>
+            </div>
+            <div class="col-md">
+              <img src="img/dog002.jpg" class="img-fluid mb-2">
+            </div>
+          </div>
 
           <p class="mb-5 main">That’s where I come in...</p>
 
@@ -41,8 +61,8 @@
             <a href="about.php" class="em-text blueLink">ABOUT ME</a>
           </h3>
 
-        </div>
-        <div class="col"></div>
+        </div> <!-- /content col -->
+        <div class="col"></div> <!-- dummy width column -->
       </div>
       <?php include ("inc/footer.php"); ?>
     </div> <!-- /mainContent -->
